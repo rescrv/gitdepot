8ee4dc7b3c552efceaace1d3db6d42ae1b2e3cba
Robert Escriva <robert@rescrv.net>
2015-10-31 09:54:12 -0400

    WIP

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(name = 'gitdepot',
+      version = '0.1.0',
+      description = 'A simple git repository management system',
+      url = 'http://rescrv.net/',
+      author = 'Robert Escriva',
+      author_email = 'robert@rescrv.net',
+      license = 'BSD',
+      keywords = 'git',
+      packages = find_packages(),
+      intall_requires = ['ply'],
+      entry_points = {
+          'console_scripts': ['gitdepot = gitdepot:main']
+          }
+      )
