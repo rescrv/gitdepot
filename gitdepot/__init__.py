1774731666e5881bff008f3f72776a24753ef3e4
Robert Escriva <robert@rescrv.net>
2015-11-06 11:11:53 -0500

    WIP

--- gitdepot/__init__.py
+++ gitdepot/__init__.py
@@ -257,7 +257,8 @@ def serve(ctx, conf, user, cmd):
             assert False
         # Now execute the git shell to do what we want
         os.environ['GITDEPOT_PRINCIPAL'] = user
-        newcmd = ' '.join([shlex.quote(x) for x in ('git', action, path)])
+        assert shlex.quote(path) == path
+        newcmd = "git {0} '{1}'".format(action, path)
         subprocess.check_call(['git', 'shell', '-c', newcmd], shell=False)
     finally:
         if erase is not None:
